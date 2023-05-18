a02911fef3faf2ba005e92a592cc5553d76a030d
Zachary Eller <37415056+zacharyeller13@users.noreply.github.com>
2023-05-18 10:33:39 -0500

    Setup boilerplate main file VMTranslator

---
+++ VMTranslator.py
@@ -0,0 +1,49 @@
+# pylint: disable=invalid-name
+"""
+Main VM Translator program. Does not conform to naming convention in order
+to comply with submission rules of the Nand2Tetris course.
+"""
+
+from argparse import ArgumentParser, Namespace
+import sys
+
+
+def initialize_argparser() -> ArgumentParser:
+    """
+    Initialize the ArgumentParser for command-line-arguments
+    """
+
+    arg_parser = ArgumentParser(
+        prog="VMTranslator", description="Translate .vm file into Hack assembly code."
+    )
+    arg_parser.add_argument(
+        "file",
+        metavar="file.vm",
+        type=str,
+        help="absolute filepath of the .vm file to be translated",
+    )
+
+    return arg_parser
+
+
+def initialize_arguments(arg_parser: ArgumentParser) -> Namespace:
+    """
+    Parse command-line-arguments
+    """
+
+    arg_namespace = arg_parser.parse_args()
+
+    if arg_namespace.file[-3:] != ".vm":
+        arg_parser.print_usage()
+        sys.exit()
+
+    return arg_namespace
+
+
+def main() -> None:
+    args = initialize_argparser()
+    file = initialize_arguments(args).file
+
+
+if __name__ == "__main__":
+    main()
