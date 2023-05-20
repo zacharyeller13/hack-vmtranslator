d4d595bffc22e7d70d26f2b5bed3057e40e94a72
Zachary Eller <37415056+zacharyeller13@users.noreply.github.com>
2023-05-19 22:15:32 -0500

    Add Command class with attributes, methods, and tests

--- tests/test_vm_parser.py
+++ tests/test_vm_parser.py
@@ -3,21 +3,67 @@ Test methods for vm_parser module
 """
 
 import os
+from pytest import raises
 
-from vm_parser import parse_file
+from vm_parser import parse_file, Command
 
 
 valid_parsed_file = ["push constant 17", "push local 2", "add", "pop argument 1"]
+
 
 def test_parse_file_basic():
     parsed_file = parse_file(f"{os.path.dirname(__file__)}/parser_test_file.vm")
     assert parsed_file == valid_parsed_file
 
+
 def test_parse_file_whitespace():
-    parsed_file = parse_file(f"{os.path.dirname(__file__)}/parser_test_file_whitespace.vm")
+    parsed_file = parse_file(
+        f"{os.path.dirname(__file__)}/parser_test_file_whitespace.vm"
+    )
     assert parsed_file == valid_parsed_file
 
 
 def test_parse_file_comments():
-    parsed_file = parse_file(f"{os.path.dirname(__file__)}/parser_test_file_comments.vm")
+    parsed_file = parse_file(
+        f"{os.path.dirname(__file__)}/parser_test_file_comments.vm"
+    )
     assert parsed_file == valid_parsed_file
+
+
+def test_command_type_push():
+    assert Command(valid_parsed_file[0]).c_type == "push"
+
+
+def test_command_type_pop():
+    assert Command(valid_parsed_file[-1]).c_type == "pop"
+
+
+def test_command_type_arithmetic():
+    assert Command(valid_parsed_file[2]).c_type == "arithmetic"
+
+
+def test_command_arithmetic_arg1():
+    assert Command("add").arg1() == "add"
+
+
+def test_command_push_arg1():
+    assert Command("push constant 17").arg1() == "constant"
+
+
+def test_command_pop_arg1():
+    assert Command("pop constant 17").arg1() == "constant"
+
+
+def test_command_push_arg2():
+    assert Command("push constant 17").arg2() == "17"
+
+
+# Currently fails as arg1() does not account for other types besides arithmetic and push/pop
+def test_command_arg1_return_raises_error():
+    with raises(TypeError):
+        Command("return").arg1()
+
+
+def test_command_arg2_return_raises_error():
+    with raises(TypeError):
+        Command("return").arg2()
--- vm_parser.py
+++ vm_parser.py
@@ -4,7 +4,66 @@ to those components.
 Ignores all whitespace and comments
 """
 
+from dataclasses import dataclass
+
 from constants import COMMENT, VAR_START
+
+
+class Command:
+    """
+    Holds a VM command with its full command, type, translation, and parts
+
+    Attributes:
+        `command` (str): the full command
+        `c_type` (str): the type of the command.  Is one of:
+            - arithmetic
+            - push
+            - pop
+            - label
+            - goto
+            - if
+            - function
+            - return
+            - call
+        `translation` (list[str]): the full translation of the command in multiple lines of
+            ASM commands
+    """
+
+    def __init__(self, command) -> None:
+        self.command: str = command
+        self.c_type: str = self._set_type(command)
+        self.translation: list[str] = []
+
+    def _set_type(self, command: str) -> str:
+        if (command_start := command.split()[0]) not in ("push", "pop"):
+            return "arithmetic"
+        return command_start
+
+    def arg1(self) -> str:
+        """
+        Returns the 1st argument to the command.  If `c_type` == "arithmetic", returns the command
+        """
+
+        if self.c_type == "return":
+            raise TypeError(f"Method not supported for Command of type {self.c_type}")
+
+        if self.c_type == "arithmetic":
+            return self.command
+        return self.command.split()[1]
+
+    def arg2(self) -> str:
+        """
+        Returns the 2nd argument to the command.  Should only run if `c_type` is in:
+            - push
+            - pop
+            - function
+            - call
+        """
+
+        if self.c_type not in ("push", "pop", "function", "call"):
+            raise TypeError(f"Method not supported for Command of type {self.c_type}.")
+
+        return self.command.split()[2]
 
 
 def parse_file(file: str) -> list[str]:
